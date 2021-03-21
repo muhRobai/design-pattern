74c000bd70ac44ed7bee3bc23109102f6602517d
muhamad robai <muhamadrobai99@gmail.com>
2021-03-21 22:23:29 +0700

    update readme and set a comment

--- factorymethod/model/model.go
+++ factorymethod/model/model.go
@@ -2,6 +2,7 @@ package model
 
 // this model layer (entity)
 
+// DeliveryProcess is must not a entity is struct to connect handler layer to use case layer
 type DeliveryProcess struct {
 	Type string
 }
